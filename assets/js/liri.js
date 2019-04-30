a210c91635d6d58db89548d7d18f305a5ec32703
Steve Travis <stevetravis11@gmail.com>
2019-04-30 14:55:27 -0400

    concert-this command is working

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -1,3 +1,36 @@
 require("dotenv").config();
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var nodeArg = process.argv;
+var moment = require("moment");
+
+// console.log(keys);
+// console.log(spotify);
+
+// concert-this command accessing Bands In Town API
+if ((nodeArg[2] = "concert-this")) {
+  var artist = nodeArg[3];
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      var bandDate = response.data[0].datetime;
+      console.log("Accessing Bands In Town API...");
+      console.log(`Venue: ${response.data[0].venue.name}`);
+      console.log(
+        `Location: ${response.data[0].venue.city}, ${
+          response.data[0].venue.country
+        }`
+      );
+      bandDate = moment(bandDate, "YYYY-MMTHH:mm:ss").format("MM/DD/YY");
+      console.log(`Date: ${bandDate}`);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
