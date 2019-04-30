1dce60835b80a863f304ec3acd1ba3d4524883cb
Steve Travis <stevetravis11@gmail.com>
2019-04-30 16:05:16 -0400

    Updated concert-this to show all concert results.

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -6,11 +6,8 @@ var spotify = new Spotify(keys.spotify);
 var nodeArg = process.argv;
 var moment = require("moment");
 
-// console.log(keys);
-// console.log(spotify);
-
-// concert-this command accessing Bands In Town API
-if ((nodeArg[2] = "concert-this")) {
+// concert-this command using Bands In Town API
+if (nodeArg[2] === "concert-this") {
   var artist = nodeArg[3];
   axios
     .get(
@@ -19,18 +16,35 @@ if ((nodeArg[2] = "concert-this")) {
         "/events?app_id=codingbootcamp"
     )
     .then(function(response) {
-      var bandDate = response.data[0].datetime;
+      var bandData = response.data;
+      if (!bandData.length) {
+        console.log(`No results found for ${artist}`);
+        return;
+      }
       console.log("Accessing Bands In Town API...");
-      console.log(`Venue: ${response.data[0].venue.name}`);
-      console.log(
-        `Location: ${response.data[0].venue.city}, ${
-          response.data[0].venue.country
-        }`
-      );
-      bandDate = moment(bandDate, "YYYY-MMTHH:mm:ss").format("MM/DD/YY");
-      console.log(`Date: ${bandDate}`);
+      console.log(`Upcoming concerts for ${artist}: `);
+      response.data.forEach(event => {
+        console.log("====================================");
+        console.log(`Venue: ${event.venue.name}`);
+        console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
+        console.log(
+          "Conecrt Date: ",
+          moment(event.datetime).format("MM/DD/YYYY")
+        );
+      });
     })
     .catch(function(error) {
       console.log(error);
     });
+  // spotify-this-song command using Spotify API
+} else if (nodeArg[2] === "spotify-this-song") {
+  var song = nodeArg[3];
+  console.log("Accessing Spotify API...");
+  spotify.search({ type: "track", query: `${song}` }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
 }
